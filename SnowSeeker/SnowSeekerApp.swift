278cb291fb368e01310d717ee6f7416a0cf9c76c
Mberndt92 <maximilian_berndt@icloud.com>
2023-05-09 15:58:22 +0900

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Maximilian Berndt on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
